027f72280fb31f725ce992fc471f2a2cc24e9a7e
Kyeonghye Jo <shienka07.ko@gmail.com>
2021-08-14 02:02:09 +0900

    Aaa 또추가

--- java/Aaa.java
+++ java/Aaa.java
@@ -2,5 +2,6 @@ public class Aaa {
     public static void main(String[] args) {
         System.out.println("git_연습중입니다.");
         System.out.println("git_제발 잘되었으면");
+        System.out.println("git_어렵ㅠ");
     }
 }
