6ae501670ef73855d84f271f4bd1b150d77b5060
Kyeonghye Jo <shienka07.ko@gmail.com>
2021-08-14 02:07:58 +0900

    Aaa 추가좀

--- java/Aaa.java
+++ java/Aaa.java
@@ -3,5 +3,6 @@ public class Aaa {
         System.out.println("git_연습중입니다.");
         System.out.println("git_제발 잘되었으면");
         System.out.println("git_어렵ㅠ");
+        System.out.println("git_쉽게하는 방법 삽니다");
     }
 }
